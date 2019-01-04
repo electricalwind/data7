e54778efea45c0cf3e479fd5dbffde69de806ca8
Zimin Chen <chen.zimin@hotmail.com>
2019-01-04 11:29:58 +0100

    All path are now handle by java.nio.file.Path

--- data7/src/main/java/data7/importer/cwe/CWEImporter.java
+++ data7/src/main/java/data7/importer/cwe/CWEImporter.java
@@ -9,9 +9,9 @@ package data7.importer.cwe;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,8 +43,8 @@ public class CWEImporter {
 
 
     public List<CWE> retrieveCWEOnline() throws IOException {
-        Misc.downloadFromURL(Resources.CWE_URL, path.getCvePath());
-        Misc.unzipping(path.getCvePath() + CWE_XML_FILE + ".zip", path.getCvePath());
+        Misc.downloadFromURL(Resources.CWE_URL, path.getCvePath().toString());
+        Misc.unzipping(path.getCvePath().resolve(CWE_XML_FILE + ".zip").toString(), path.getCvePath().toString());
         List<CWE> cweList = CWEParser.parse(path);
         new Exporter(path).saveCWEList(cweList);
         return cweList;
